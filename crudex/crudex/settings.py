04b1a6ac33be53ad6df1ca8475fed0d5f28299a7
MikeSoft007 <mekpenyong2@gmail.com>
2021-06-22 10:52:11 +0000

    added models

--- crudex/crudex/settings.py
+++ crudex/crudex/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'crudex.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'djangodb',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
